104d300e96804711692bcfd4dfcf5c74d5ad82cf
ingridcavalli <106964736+ingridcavalli@users.noreply.github.com>
2023-03-06 11:40:11 -0300

    Add files via upload

---
+++ Galileu/js/supla.js
@@ -0,0 +1,33 @@
+
+// Declaração de variáveis 
+let nome = 'Ingrid'
+let numeroInteiro = 1245
+let numeroFlutuante = 25.66
+let condicao= true
+
+const Pi = 3.1415
+console.log(Pi)
+
+// Operadores matemáticos
+let soma = 1 + 1
+let subs = 1 - 1
+let mults = 1 * 1
+let divs = 1 / 1
+
+// Operacionais relacionados 
+let compara = soma == subs
+let melhor = 1 == 2
+
+let diferente = soma !== divs
+let  maior = subs >= soma
+let menor = divs <= subs
+
+// Operadores lógicos 
+let eee = 3 & 5
+let ou = 10 || 9 
+let nott = !ou
+
+let mensagem = 'Olá, seja bem-vindo!!!'
+ // DOM - Document object Model 
+ //document.write(mensagem + '<br>')
+ //alert(mensagem + '\n')
